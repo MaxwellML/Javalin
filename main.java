a5b3e1092daee8306dff1f1af54fa3de55724383
MaxwellML <cmdp7@outlook.com>
2026-03-09 20:30:47 +0000

    Changed the name from start to main and added my first function.

---
+++ main.java
@@ -0,0 +1,10 @@
+//let's do this!
+
+public class main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int i : numbers) {
+            System.out.println(i);
+        }
+    }
+}
