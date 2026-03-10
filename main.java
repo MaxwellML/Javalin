5089fa866fb32bed99434a212335e3eb9a0ce593
Connor Dean-Pijuan <cmdp7@outlook.com>
2026-03-10 09:00:24 +0000

    Update main.java
    Main Java now uses the writefile function.

--- main.java
+++ main.java
@@ -2,9 +2,6 @@
 
 public class main {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
-        for (int i : numbers) {
-            System.out.println(i);
-        }
+        WriteFile.writefile();
     }
 }
